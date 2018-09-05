20626df85addc9f9a199a90c33cd2cf8e1605890
LovZsolt <lovzsolt@gmail.com>
2018-09-05 17:42:31 +0200

    első commit

---
+++ main.js
@@ -0,0 +1,58 @@
+let szamok = [];
+
+const starTime = new Date().getTime();
+
+let stat = new Map();
+let tempSzamok = szamok;
+const MAX_NUMBERS = 91;
+const DRAW_COUNT = 5;
+let i = 1;
+while (i < MAX_NUMBERS) {
+    szamok.push(i++);
+}
+
+for (let x = 0; x < 100000; x++) {
+    tempSzamok = szamok;
+    let draws = [];
+    let i = 1;
+
+    for (let j = 0; j < DRAW_COUNT; j++) {
+        draws.push(sorsolas());
+        // console.log('számok: '+szamok.join(','));
+
+    }
+     /*console.log('Lottószámok: ' + draws.join(','));
+    console.log('-------------------------------------');
+    itt kellene összegyűjteni a */
+    draws.map(e => {
+        let ertek = stat.get(e);
+        if (ertek == undefined) {
+            stat.set(e, 1);
+        } else {
+            stat.set(e, ++ertek);
+        }
+    });
+}
+
+stat[Symbol.iterator] = function* () {
+    yield* [...this.entries()].sort((a, b) => b[1] - a[1]);
+}
+//console.log(stat)
+let counter = 0;
+for (let [key, value] of stat) {     // get data sorted
+    console.log(`szám: ${key}, érték: ${value}`);
+    if(counter === 5)
+        break;
+    counter++;
+}
+
+const endTime = new Date().getTime();
+console.log(`idő: ${(endTime-starTime)/1000} s`);
+
+function sorsolas() {
+    const index = Math.floor(Math.random() * Math.floor(tempSzamok.length));
+    const sorsoltSzam = tempSzamok[index];
+    //console.log('sorsoltszám: '+sorsoltSzam);
+    tempSzamok = tempSzamok.filter(x => x != sorsoltSzam);
+    return sorsoltSzam;
+}
